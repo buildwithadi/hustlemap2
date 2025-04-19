e18a5625f952ba82d289c4d37948a2178833d9bc
buildwithadi <adityarawat.online@gmail.com>
2025-04-19 17:05:30 +0530

    Deployement - 4

--- settings.py
+++ settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-your-secret-key-here')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+DEBUG = 'False'
 
 # Set ALLOWED_HOSTS
 ALLOWED_HOSTS = ['hustlemap.onrender.com', 'localhost', '127.0.0.1']
